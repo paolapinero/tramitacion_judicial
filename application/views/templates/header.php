21b7d0a8854f1a9113da3e16f271b0684185db21
Paola Alejandra Piñero Gandarillas <ppinero@serbanc.cl>
2015-11-23 11:03:09 -0300

    arreglando layout

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,17 +7,30 @@
     <link rel="stylesheet" href="<?php echo base_url("/assets/bootstrap/css/bootstrap.css"); ?>" />
    
 </head>
-<body>
+<body style="padding-top: 70px;padding-bottom: 30px;">
 	<div id="">
 		<div id="header">
             <div class="container-fluid">  
-                <nav class="navbar navbar-inverse navbar-fixed-top">
-                    <div class="container-fluid">
-                        <div class="navbar-header">
-                            <img src="<?=base_url()?>/img/inicio.png" style="float:left;margin-right:5px" />
-                            <a class="navbar-brand" href="<?=  base_url()?>"> Tramitación Judicial</a>
-                        </div>
+               <nav class="navbar navbar-inverse navbar-fixed-top">
+              <div class="container-fluid">
+                <div class="navbar-header">
+                    <img style="float: left; margin-right: 5px;" src="<?php echo base_url("/img/inicio.png") ?>">
+                    <a class="navbar-brand" href="<?php echo base_url() ?>">Egresos Judiciales</a>
                 </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                
+                  <!-- <ul class="nav navbar-nav navbar-left">
+                        <li><a href="?">Crear Solicitud</a></li>
+                        <li><a href="?page=pendientes">Ver Solicitudes</a></li>
+                  </ul>
+                  
+                  
+                   <ul class="nav navbar-nav navbar-right">
+                        <li><a href="logout.php">Salir</a></li>
+                   </ul> -->
+                </div>
+              </div>
+            </nav>
             </nav>
 		</div>
 		</div>
